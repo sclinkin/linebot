3650d5e4452bf4f4ad9ece2ed48764d45900a308
20088 <ian@addcn.com>
2017-06-28 11:17:43 +0800

    新增樣板

--- callback.php
+++ callback.php
@@ -14,10 +14,9 @@
 		var $channel_token;
 
 		function __construct(){
-			$this->channel_token="";
+			$this->channel_token="your token";
 
 		}
-		
 		/* 發送訊息 */
 		function sendMessage($to , $message) {
 				$url = "https://api.line.me/v2/bot/message/push";
@@ -40,7 +39,67 @@
 				curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
 				$result = curl_exec($curl);
 				curl_close($curl);
-				print_r($result);
+				print '<pre>';
+				print_r(json_decode($result));
+		}
+		/* 發送訊息 */
+		function sendTemplateMessage($to , $message) {
+							$url = "https://api.line.me/v2/bot/message/push";
+							$header = array("Content-Type: application/json; charser=UTF-8", "Authorization:Bearer " . $this->channel_token);
+							$data = array("to" => $to, 
+					  "messages"=>(array(array("type"=>"template",
+								   "altText"=>"test hi",
+								   "template"=>array("type"=>"confirm",
+											 "text"=>"你要跟我說話嗎",
+										 "actions"=>array(array("type"=>"uri","label"=>"YES","uri"=>"https://www.101vip.com.tw"),
+												    array("type"=>"uri","label"=>"NO","uri"=>"https://www.101.com.tw/"),
+													)
+										) 
+								   ))));
+
+							$context = $data;
+
+							$curl = curl_init();
+							curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
+							curl_setopt($curl, CURLOPT_SSL_VERIFYPEER,0);
+
+
+							curl_setopt($curl, CURLOPT_POST, true);
+							curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode( $context ));
+
+							curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+							curl_setopt($curl, CURLOPT_TIMEOUT, 5 );
+							curl_setopt($curl, CURLOPT_URL, $url);
+							curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+							$result = curl_exec($curl);
+							curl_close($curl);
+							print '<pre>';
+							print_r(json_decode($result));
+		}
+		/* 回應訊息 */
+		function replyMessage($to , $message) {
+				$url = "https://api.line.me/v2/bot/message/reply";
+				$header = array("Content-Type: application/json; charser=UTF-8", "Authorization:Bearer " . $this->channel_token);
+				$data = array("replyToken" => $to, "messages"=>(array(array("type"=>"text","text"=>$message))));
+
+				$context = $data;
+								
+				$curl = curl_init();
+				curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
+				curl_setopt($curl, CURLOPT_SSL_VERIFYPEER,0);
+			
+				
+				curl_setopt($curl, CURLOPT_POST, true);
+				curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode( $context ));
+				
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($curl, CURLOPT_TIMEOUT, 5 );
+				curl_setopt($curl, CURLOPT_URL, $url);
+				curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+				$result = curl_exec($curl);
+				curl_close($curl);
+				print '<pre>';
+				print_r(json_decode($result));
 		}
 	}
 ?>
